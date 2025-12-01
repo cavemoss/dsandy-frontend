fa1cde4e3b4e9496049c77db6270af55056d580f
Александр Шушаков <a.shushakov@gnzs.ru>
2025-12-01 17:36:39 +0800

    deploy: fix error

--- src/app/admin/(dashboard)/layout.tsx
+++ src/app/admin/(dashboard)/layout.tsx
@@ -24,7 +24,7 @@ export default function AdminDashboardLayout({ children }: Readonly<{ children: 
         <main className="w-full">
           <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
             <SidebarTrigger className="-ml-1" />
-            <Separator orientation="vertical" className="mr-2 data-[orientation=vertical]:h-4" />
+            <Separator className="mr-2 data-[orientation=vertical]:h-4" />
             <Breadcrumb>
               <BreadcrumbList>
                 <BreadcrumbItem className="hidden md:block">
